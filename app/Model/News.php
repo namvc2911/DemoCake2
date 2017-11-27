bc00e4fa57b2395e93f82b1bcb7db504ca2e7cc2
namvu.ntsoft <namvu.ntsoft@gmail.com>
2017-11-27 17:19:08 +0700

    fix

--- app/Model/News.php
+++ app/Model/News.php
@@ -1,11 +1,7 @@
 <?php 
 	class News extends AppModel{
 		var $name = "News";
-		 // public $actsAs = array(
-   //     'Translate' => array(
-   //          'title', 'content'
-   //      )
-   //  );
+		 
 		public $belongsTo = array(
 			'User'=>array(
 			'className' =>'User',
@@ -13,6 +9,11 @@
 			)
 
 		);
+		public $actsAs = array(
+       'Translate' => array(
+            'title', 'content'
+        )
+    );
 		// $this->News->locale  =  'en' ; 
 		// $results  =  $this ->Post ->find ( 'first' ,  array ( 
   //   	'conditions'  =>  mảng ( 'News.id'  => $id ) 
