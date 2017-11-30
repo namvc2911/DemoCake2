1f7ef6c5566eeda27b611b9d2e067f56d15b9b55
namvu.ntsoft <namvu.ntsoft@gmail.com>
2017-11-30 13:05:27 +0700

    cx

---
+++ app/Config/facebook.php
@@ -0,0 +1,15 @@
+<?php
+/**
+  * Get an api_key and secret from facebook and fill in this content.
+  * save the file to app/Config/facebook.php
+  */
+  $config = array(
+  	'Facebook' => array(
+  		'appId' => '380343135634066',
+  		'apiKey' => '67cdc261d55441592cae87be04cebbc4',
+  		'secret' => '67cdc261d55441592cae87be04cebbc4',
+  		'cookie' => true,
+  		'locale' => 'en_US',
+  		)
+  	);
+?>
